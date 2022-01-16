8488235e48c96abd61ebba51008c0055968c77dc
hungrytech <xorals9448@gmail.com>
2022-01-16 18:29:15 +0900

    feat: InvalidValueException 추가

---
+++ src/main/java/com/comeon/study/common/exception/InvalidValueException.java
@@ -0,0 +1,8 @@
+package com.comeon.study.common.exception;
+
+public class InvalidValueException extends ApplicationException {
+
+    public InvalidValueException(String message) {
+        super(message);
+    }
+}

de186fd44807319fc994dc6e26c3720a8722bbe7
hungrytech <xorals9448@gmail.com>
2022-01-16 22:31:20 +0900

    refactor : AuthHeader final class로 변경

--- src/main/java/com/comeon/study/common/config/security/jwt/AuthHeader.java
+++ src/main/java/com/comeon/study/common/config/security/jwt/AuthHeader.java
@@ -1,6 +1,6 @@
 package com.comeon.study.common.config.security.jwt;
 
-public class AuthHeader {
+public final class AuthHeader {
 
     public static final String DECIDED_HEADER_NAME = "Bearer ";
 }

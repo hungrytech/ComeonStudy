e346fba6fd7ada0cf8c615a366874025a9d07e9f
hungrytech <xorals9448@gmail.com>
2022-01-21 22:48:12 +0900

    refactor : refresh token 만료일, 발행 메서드 추가

--- src/main/java/com/comeon/study/common/config/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/comeon/study/common/config/security/jwt/JwtTokenProvider.java
@@ -13,11 +13,15 @@ public class JwtTokenProvider extends JwtTokenUtil {
 
     private final long expirationTime;
 
+    private final long refreshTokenExpirationTime;
+
     public JwtTokenProvider(
             @Value("${jwt.secret-key}") String secretKey,
-            @Value("${jwt.expiration-time}") long expirationTime) {
+            @Value("${jwt.expiration-time}") long expirationTime,
+            @Value("${jwt.refresh-expiration-time}") long refreshTokenExpirationTime) {
         super(secretKey);
         this.expirationTime = expirationTime;
+        this.refreshTokenExpirationTime = refreshTokenExpirationTime;
     }
 
     public String generateAccessToken(Long memberId) {
@@ -28,6 +32,18 @@ public class JwtTokenProvider extends JwtTokenUtil {
                 .setClaims(claims)
                 .setIssuedAt(date)
                 .setExpiration(new Date(date.getTime() + expirationTime))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public String generateRefreshToken(Long memberId) {
+        Claims claims = createClaims(memberId);
+        Date date = new Date();
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(date)
+                .setExpiration(new Date(date.getTime() + refreshTokenExpirationTime))
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }

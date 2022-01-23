ca0530a1d5ec108a71bbb867761f7aa3a61ac529
hungrytech <xorals9448@gmail.com>
2022-01-23 12:34:41 +0900

    test : 로그인 실패 테스트 작성

--- src/test/java/com/comeon/study/member/application/MemberServiceTest.java
+++ src/test/java/com/comeon/study/member/application/MemberServiceTest.java
@@ -1,14 +1,15 @@
 package com.comeon.study.member.application;
 
 import com.comeon.study.common.config.security.jwt.JwtTokenProvider;
-import com.comeon.study.member.domain.refreshtoken.RefreshToken;
 import com.comeon.study.member.domain.Member;
+import com.comeon.study.member.domain.refreshtoken.RefreshToken;
 import com.comeon.study.member.domain.repository.MemberRepository;
 import com.comeon.study.member.domain.repository.RefreshTokenRepository;
 import com.comeon.study.member.dto.MemberJoinRequest;
 import com.comeon.study.member.dto.MemberJoinResponse;
 import com.comeon.study.member.dto.MemberLoginRequest;
 import com.comeon.study.member.dto.MemberLoginResponse;
+import com.comeon.study.member.exception.NotMatchLoginValueException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,6 +22,7 @@ import java.util.Optional;
 import static com.comeon.study.member.fixture.MemberFixture.*;
 import static com.comeon.study.member.fixture.RefreshTokenFixture.TEST_REFRESH_TOKEN;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.any;
@@ -81,10 +83,37 @@ class MemberServiceTest {
         MemberLoginResponse memberLoginResponse = memberService.signIn(memberLoginRequest);
 
         // then
-
         assertAll(
                 () -> assertThat(memberLoginResponse.getNickName()).isEqualTo(TEST_MEMBER_LOGIN_NICKNAME),
                 () -> assertThat(memberLoginResponse.getRefreshToken()).isEqualTo(TEST_REFRESH_TOKEN.getValue())
         );
+    }
+
+    @Test
+    void 로그인_실패_찾는_회원이_없는경우() {
+        // given
+        MemberLoginRequest memberLoginRequest = new MemberLoginRequest(TEST_MEMBER_LOGIN_EMAIL, TEST_MEMBER_LOGIN_PASSWORD);
+
+        // when
+        given(memberRepository.findMemberByEmail(anyString())).willReturn(Optional.empty());
+
+        // then
+        assertThatThrownBy(() -> memberService.signIn(memberLoginRequest))
+                .isInstanceOf(NotMatchLoginValueException.class);
+
+    }
+
+    @Test
+    void 로그인_실패_비밀번호가_다를경우() {
+        // given
+        MemberLoginRequest memberLoginRequest = new MemberLoginRequest(TEST_MEMBER_LOGIN_EMAIL, TEST_MEMBER_LOGIN_PASSWORD);
+
+        // when
+        given(memberRepository.findMemberByEmail(anyString())).willReturn(Optional.of(TEST_LOGIN_MEMBER));
+        given(passwordEncoder.matches(anyString(), anyString())).willReturn(false);
+
+        // then
+        assertThatThrownBy(() -> memberService.signIn(memberLoginRequest))
+                .isInstanceOf(NotMatchLoginValueException.class);
     }
 }
--- src/test/java/com/comeon/study/member/domain/repository/MemberRepositoryTest.java
+++ src/test/java/com/comeon/study/member/domain/repository/MemberRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.comeon.study.member.domain.repository;
 
 import com.comeon.study.member.domain.Member;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -52,4 +53,16 @@ class MemberRepositoryTest {
         assertThat(member.getEmail()).isEqualTo(email);
     }
 
+    @DisplayName("이메일이 알맞은 회원이 없는경우")
+    @Test
+    void 회원이메일_이용한_로그인_실패_찾는_회원이_없는_경우() {
+        // given
+        String email = "test2@naver.com";
+
+        // when
+        Optional<Member> memberOptional = memberRepository.findMemberByEmail(email);
+
+        //then
+        assertThat(memberOptional.isPresent()).isFalse();
+    }
 }

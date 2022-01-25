ae901f9c943e13ee30f315221980c31bca5f72b6
hungrytech <xorals9448@gmail.com>
2022-01-25 23:16:55 +0900

    refactor : rest-assured을 사용하도록 변경

---
+++ src/test/java/com/comeon/study/common/AcceptanceTest.java
@@ -0,0 +1,20 @@
+package com.comeon.study.common;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AcceptanceTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+}
---
+++ src/test/java/com/comeon/study/member/acceptance/LoginAcceptanceTest.java
@@ -0,0 +1,92 @@
+package com.comeon.study.member.acceptance;
+
+import com.comeon.study.common.AcceptanceTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+import static com.comeon.study.member.fixture.MemberFixture.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+
+public class LoginAcceptanceTest extends AcceptanceTest {
+
+    @Test
+    void 회원가입_성공() {
+        // given
+        given()
+                .contentType(ContentType.JSON)
+                .body(MEMBER_JOIN_REQUEST_JSON)
+
+                // when
+                .when()
+                .post("/api/v1/join")
+
+                // then
+                .then()
+                .statusCode(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    void 회원가입_실패_이미_회원인_경우() {
+        // given
+        given()
+                .contentType(ContentType.JSON)
+                .body(JOINED_TEST_MEMBER_JSON)
+
+                // when
+                .when()
+                .post("/api/v1/join")
+
+                // then
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .assertThat()
+                .body("success", equalTo(false))
+                .body("message", equalTo("이미 가입된 회원입니다."));
+    }
+
+    @Test
+    void 회원가입_실패_잘못된_이메일_입력시() {
+        // given
+        given()
+                .contentType(ContentType.JSON)
+                .body(INVALID_MEMBER_JOIN_REQUEST_JSON)
+
+                // when
+                .when()
+                .post("/api/v1/join")
+
+                // then
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .assertThat()
+                .body("success", equalTo(false))
+                .body("messages", equalTo(List.of("잘못된 이메일 양식입니다.")));
+    }
+
+    @Test
+    void 로그인_성공() {
+        //given
+        given()
+                .contentType(ContentType.JSON)
+                .body(LOGIN_REQUEST_JSON)
+
+                // when
+                .when()
+                .post("/api/v1/login")
+
+                // then
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .assertThat()
+                .body("data.nickName", equalTo(TEST_MEMBER_LOGIN_NICKNAME))
+                .body("data.accessToken", notNullValue())
+                .body("data.refreshToken", notNullValue());
+    }
+
+}
